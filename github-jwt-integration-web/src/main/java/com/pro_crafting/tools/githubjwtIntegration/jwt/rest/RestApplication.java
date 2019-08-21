239b507f5369ea630024261c168360b5c1328585
Martin Panzer <postremus1996@googlemail.com>
2019-08-21 21:27:09 +0200

    change base path

--- github-jwt-integration-web/src/main/java/com/pro_crafting/tools/githubjwtIntegration/jwt/rest/RestApplication.java
+++ github-jwt-integration-web/src/main/java/com/pro_crafting/tools/githubjwtIntegration/jwt/rest/RestApplication.java
@@ -3,7 +3,7 @@ package com.pro_crafting.tools.githubjwtIntegration.jwt.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath(RestApplication.VERSION_PATH)
+@ApplicationPath("jwt-api/" + RestApplication.VERSION_PATH)
 public class RestApplication extends Application {
     public static final String VERSION_PATH = "v1/";
 }
